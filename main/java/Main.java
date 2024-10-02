d96095af89dfaab7fa799655b4f61ca8afd3c0d9
cd-x <whowantsthisemailid@gmail.com>
2024-10-02 22:33:53 +0530

    exceptions, TryWithResource, Closeable

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,6 +1,8 @@
 package main.java;
 
 import main.java.ExceptionHandling.DivideByZero;
+import main.java.collections.implementation.Stack;
+import main.java.collections.interfaces.IStack;
 import main.java.implementation.DemoInnerClass;
 import main.java.implementation.DummyAutoCloseable;
 import main.java.implementation.SubClass;
@@ -11,14 +13,15 @@ import java.util.Scanner;
 public class Main {
     //static FileInputStream fi;
     static public void main(String[] args) {
-        try(
-                //FileInputStream fi = new FileInputStream("/user/downloads");
-                //Scanner sc = new Scanner(System.in);
-                DummyAutoCloseable da = new DummyAutoCloseable();
-                ){
-            da.toString();
+        IStack st = new Stack(2);
+        try{
+            st.push(1);
+            st.push(2);
+            st.push(3);
         }
-        catch (Exception e){}
+        catch (Exception e){
+            System.out.println(e);
+        }
     }
 }
 
