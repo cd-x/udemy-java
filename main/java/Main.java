a4da2aeee32fb09a96f5775452fdb0ad49f6a518
cd-x <whowantsthisemailid@gmail.com>
2024-10-16 06:33:30 +0530

    java.lang.String

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,32 +1,15 @@
 package main.java;
 
+import main.java.exploreLang.Wrapper;
 import main.java.threading.*;
 
 public class Main {
     static public void main(String[] args) {
-        MyData d = new MyData();
-        Producer p1 = new Producer(d);
-        Producer p2 = new Producer(d);
-        Producer p3 = new Producer(d);
-        Consumer c1 = new Consumer(d);
-        Consumer c2 = new Consumer(d);
-        Consumer c3 = new Consumer(d);
-        p1.start();
-        p2.start();
-        p3.start();
-        c1.start();
-        c2.start();
-        c3.start();
-        try{
-            p1.join();
-            p2.join();
-            p3.join();
-            c1.join();
-            c2.join();
-            c3.join();
-        }catch (InterruptedException ie) {
-            ie.printStackTrace();
-        }
+        String s = "Hello";
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.chars();
+        StringBuilder sbl = new StringBuilder("builder");
+        System.out.println(sb);
     }
 }
 
---
+++ main/java/exploreLang/Wrapper.java
@@ -0,0 +1,11 @@
+package main.java.exploreLang;
+
+import java.util.Objects;
+
+public class Wrapper {
+    public void run(){
+        Float f = Float.valueOf(1.23f);
+        Class<?> x = f.getClass();
+        
+    }
+}
