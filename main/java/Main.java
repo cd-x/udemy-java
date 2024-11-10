14a0dac7044282084d90d87ec525a057475effce
cd-x <whowantsthisemailid@gmail.com>
2024-11-10 21:49:06 +0530

    LRU

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,15 +1,22 @@
 package main.java;
 
 
-import main.java.collections.implementation.CustomHashMap;
-import main.java.collections.interfaces.IMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Main {
     static public void main(String[] args) {
-        IMap<String, String> map = new CustomHashMap<>();
-        map.put("name", "rajrishi");
-        map.put("place", "Gurugram");
-        map.put("college", "NITT");
-        System.out.println(map.get("college"));
+        LinkedHashMap<Integer, String> linkedHashMap = new LinkedHashMap<>(3, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry e) {
+                return size() > 3;
+            }
+        };
+        linkedHashMap.put(23, "Hello");
+        linkedHashMap.put(46, "world");
+        linkedHashMap.put(1, "first");
+        linkedHashMap.put(2, "second");
+        linkedHashMap.get(46);
+        System.out.println(linkedHashMap);
     }
 }
