5d6d70a058f5e891d5f3c3944073360d0d53710c
cd-x <whowantsthisemailid@gmail.com>
2024-11-30 00:25:58 +0530

    print while interrupted

--- main/java/Main.java
+++ main/java/Main.java
@@ -1,9 +1,27 @@
 package main.java;
 
 
-public class Main {
+import java.io.IOException;
+import java.util.Scanner;
 
-    static public void main(String[] args) {
-        
+public class Main {
+    static public void main(String[] args) throws IOException {
+        final boolean[] isRunning = {true};
+        Thread keyListener = new Thread(() -> {
+            Scanner sc = new Scanner(System.in);
+            sc.nextLine();
+            isRunning[0] = false;
+            sc.close();
+        });
+        keyListener.start();
+        while (isRunning[0]) {
+            System.out.println("printing...");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+                break;
+            }
+        }
     }
 }
