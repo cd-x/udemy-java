92d3fe5e4b541cb031c382ea6603437244881817
rajrishivishwakarma <whowantsthisemailid@gmail.com>
2025-02-18 18:10:30 +0530

    wait till complete

--- main/java/threading/jenkov/VirtualThread.java
+++ main/java/threading/jenkov/VirtualThread.java
@@ -4,9 +4,16 @@ import java.util.stream.IntStream;
 
 public class VirtualThread {
     public void spawnVirtualThreads() throws InterruptedException {
-        IntStream.range(0,10).forEach(count -> Thread.startVirtualThread(() ->{
-            System.out.println("Executing virtual:"+Thread.ofVirtual().name("VT-", 1));
-        }));
+        IntStream.range(0,10).forEach(count -> {
+            Thread curThread =  Thread.startVirtualThread(() -> {
+                System.out.println("Executing virtual:" + Thread.ofVirtual().name("VT-", 1));
+            });
+            try {
+                curThread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
     public void spawnVirtualThreads2() throws InterruptedException {
         IntStream.range(0,100).forEach(count -> Thread.ofVirtual().start(() ->{
